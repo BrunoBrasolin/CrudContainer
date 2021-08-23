e7b89bb09dde86ae9a21c81a1f7df4c733d4ca1b
Bruno Brasolin <brunobrasolinc@gmail.com>
2021-08-23 14:29:41 -0300

    validacoes

--- Models/Container.cs
+++ Models/Container.cs
@@ -3,12 +3,29 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CrudContainer.Models
 {
-  public class Container {
+  public class Container
+  {
     public int Id { get; set; }
+
+    [StringLength(11)]
+    [Required]
+    [RegularExpression("^[A-Z]{4}[0-9]{7}$")]
     public string Number { get; set; }
+
+    [StringLength(255)]
+    [Required]
     public string Client { get; set; }
+
+    [Range(0, 1)]
+    [Required]
     public int Type { get; set; }
+
+    [Range(0, 1)]
+    [Required]
     public int Status { get; set; }
+
+    [Range(0, 1)]
+    [Required]
     public int Category { get; set; }
   }
 }
